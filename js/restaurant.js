8af58ba1f4d1c938dbca34ba8706d4aaadd344e1
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-07-05 10:23:37 +0900

    20220705

---
+++ js/restaurant.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.main_visual01').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+    $('.main_visual02 .main_visual02_slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+})
